434b7ff6d6523b12c7cfa640fca306306c555c54
Alexxs <irreligious86@gmail.com>
2021-12-19 21:12:51 +0200

    complete main usability

--- script.js
+++ script.js
@@ -1,31 +1,29 @@
-const value1 = document.querySelector('#val1');
-const value2 = document.querySelector('#val2');
-const operator = document.querySelector('#operator');
+
 const result = document.querySelector('#result');
 const runBtn = document.querySelector('.run-button');
 
-
-
-
 const calc = (a, b, operator) => {
-    switch(operator) {
-        // case '+': return a+b;
-        case '+': return a+b;
-        case '-': return a-b;
-        case '*': return a*b;
-        case '/': return a/b;
-        default: return "Something wrong";
+    switch (operator) {
+        case '+':
+            return +a + +b;
+        case '-':
+            return a - b;
+        case '*':
+            return a * b;
+        case '/':
+            return a / b;
+        default:
+            return "Something wrong";
     }
 }
 
 const runBtnHandler = (event) => {
     event.preventDefault();
-    console.log(value1, value2, operator, result, runBtn);
-    console.log(event)
-    console.log(calc(value1, value2, operator));
-    result.value = calc(value1, value2, operator);
+    const value1 = document.querySelector('#val1').value;
+    const value2 = document.querySelector('#val2').value;
+    const select = document.querySelector('#operator');
+    const selectValue = select.options[select.selectedIndex].value;
+    result.value = calc(value1, value2, selectValue);
 }
-
-
 
 runBtn.addEventListener('click', runBtnHandler)
